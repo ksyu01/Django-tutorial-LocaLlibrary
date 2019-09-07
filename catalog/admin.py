764af9f0c552cbe2d38fbed9a45f015097d4ad53
ksyu01 <ksyu01@yahoo.com>
2019-09-07 21:25:01 +0300

    Добавление доп. полей из таблиц; группировка данных на странице; добавление доп. полей из связанных таблиц

--- catalog/admin.py
+++ catalog/admin.py
@@ -13,21 +13,41 @@ admin.site.register(Genre)  # SKurbatskiy 20190907 Создаём собстве
 class AuthorAdmin(admin.ModelAdmin):
     # Перечисление полей, которые будут показываться вместо предыдущего __str__ из {self.last_name}, {self.first_name}
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    # То, что сгруппирована в tuple, показывается горизонтально рядом друг с другом (не друг под другом)
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
 
+# Для показа данных по Instance в BookAdmin
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    # Сколько добавить пустых Instance для заполнения (по умолчанию почему-то ставит 3)
+    extra = 1
+
 # Точно такой же эффект, как для Author, дают декораторы:
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
-
+    # Ссылка на определенный раньше класс для показа Instance в BookAdmin
+    inlines = [BooksInstanceInline]
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     # Добавление фильтров для вывода
     list_filter = ('status', 'due_back')
+
+    # Если хотим разделить данные по секциям
+    # Первым идёт название секции (или None, если название не нужно)
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
