bbbf2ba2c92a8022bf3ec32ffe924f1837bf1c26
ksyu01 <ksyu01@yahoo.com>
2019-09-08 00:03:59 +0300

    Создание view для страницы index

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    # в параметрах URL pattern
+    # функция, которую нужно будет искать в модуле views и вызывать её
+    # name - имя этого конкретного URL-маппинга
+    path('', views.index, name='index'),
 ]
