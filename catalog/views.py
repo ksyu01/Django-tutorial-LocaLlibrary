ac9401e9fbb0c027ee7067b6fa4e290d162765b2
ksyu01 <ksyu01@yahoo.com>
2019-09-08 23:03:14 +0300

    Sessions: создан ключ num_visits для показа, сколько раз заходили на страницу в рамках этой сессии

--- catalog/views.py
+++ catalog/views.py
@@ -21,6 +21,10 @@ def index(request):
     genres_contain = Genre.objects.filter(name__contains='я').count()
     books_contain = Book.objects.filter(title__contains='я').count()
 
+    # Number of visits to this view, as counted in the session variable. Количество визитов
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -28,6 +32,7 @@ def index(request):
         'num_authors': num_authors,
         'genres_contain': genres_contain,
         'books_contain': books_contain,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
