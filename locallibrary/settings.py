d1c46813f73f0fc507e7530564ef7a37fdc68288
ksyu01 <ksyu01@yahoo.com>
2019-09-09 22:21:32 +0300

    В base_generic добавлено отображение текущего пользователя с возможностью логина и разлогина и возвратом после этого на эту же страницу

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
-LOGIN_REDIRECT_URL = '/'
+# LOGIN_REDIRECT_URL = '/'
 
 # показ отправленных email в консоль
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
